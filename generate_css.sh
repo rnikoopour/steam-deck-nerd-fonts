c4a32e35fe766a355f882bbc061ba5227bc9367a
Reza Nikoopour <rnikoopour@gmail.com>
2024-01-18 23:32:16 -0800

    adding quotes to fix 3270 issue

--- generate_css.sh
+++ generate_css.sh
@@ -3,11 +3,11 @@ for font in `/bin/ls theme/nerd-fonts/*.ttf`; do
     font_name=`echo $font | awk -F'.' '{print$2}' | sed 's/NerdFont-Regular//'`;
 
     echo "@font-face {" > ./theme/font-css/${font_name}.css
-    echo "  font-family: ${font_name};" >> ./theme/font-css/${font_name}.css
+    echo "  font-family: \"${font_name}\";" >> ./theme/font-css/${font_name}.css
     echo "  src: url(\"/themes_custom/Nerd%20Fonts/nerd-fonts/${file_name}\");" >> ./theme/font-css/${font_name}.css
     echo "}" >> ./theme/font-css/${font_name}.css
     echo "" >> ./theme/font-css/${font_name}.css
     echo "* {" >> ./theme/font-css/${font_name}.css
-    echo "  font-family: ${font_name} !important;" >> ./theme/font-css/${font_name}.css
+    echo "  font-family: \"${font_name}\" !important;" >> ./theme/font-css/${font_name}.css
     echo "}" >> ./theme/font-css/${font_name}.css
 done
